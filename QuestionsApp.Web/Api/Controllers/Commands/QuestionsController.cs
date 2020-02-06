6dd8104067ca902d17fc366bce63bae10e8bccfd
Volkmar Rigo <volkmar.rigo@infominds.eu>
2020-02-06 14:19:46 +0100

    Create solution, folders and classes

---
+++ QuestionsApp.Web/Api/Controllers/Commands/QuestionsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionsApp.Web.Api.Controllers.Commands
+{
+    public class QuestionsController
+    {
+    }
+}
---
+++ QuestionsApp.Web/Api/Models/Question.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionsApp.Web.Api.Models
+{
+    public class Question
+    {
+    }
+}
