c728656d96e64d525c2d26326a39d2d425853c2b
Volkmar Rigo <volkmar.rigo@infominds.eu>
2020-02-06 15:01:50 +0100

    Activated MVC for WebApi

--- QuestionsApp.Web/Startup.cs
+++ QuestionsApp.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace QuestionsApp.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Registration and configuration of the MVC Framework
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +32,12 @@ namespace QuestionsApp.Web
 
             app.UseEndpoints(endpoints =>
             {
+                // Activate MVC Controllers for WebApi
+                endpoints.MapControllers();
+
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Error");
                 });
             });
         }
